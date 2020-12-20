9e6aac3c3dfb050d24ab8da6ba4eb7690529aaef
Joseph Abad <75592216+coolstoryjoe-bro@users.noreply.github.com>
2020-12-19 21:05:32 -0800

    Add files via upload

---
+++ itemValues.py
@@ -0,0 +1,29 @@
+# These are the item = money exchange rate.
+item_values = {
+    'cqc weapons': {
+        'longsword': '',
+        'mace': '',
+        'shortsword': '',
+        'dagger': '',
+        'staff': '',
+    },
+    'defensive items': {
+        'short shield': '',
+        'tall shield': '',
+
+    },
+    'ranged weapons': {
+
+    },
+    'animal drops': {
+        'boar hide': 5
+    },
+    'monster drops': {
+
+    },
+    'mineral drops': {
+
+    },
+
+}
+
