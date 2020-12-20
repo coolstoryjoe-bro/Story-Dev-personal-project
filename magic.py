3c9f7a1d039d683e18541f191f41f271a6f38940
Joseph Abad <75592216+coolstoryjoe-bro@users.noreply.github.com>
2020-12-19 21:05:39 -0800

    Add files via upload

---
+++ magic.py
@@ -0,0 +1,13 @@
+class magic:
+    def __init__(self, fire, ice, water, earth, void, light, dark, electric, necromancy, goddess, demon,):
+        self.fire = fire
+        self.ice = ice
+        self.water = water
+        self.earth = earth
+        self.void = void
+        self.light = light
+        self.dark = dark
+        self.electric = electric
+        self.necromancy = necromancy
+        self.goddess = goddess
+        self.demon = demon
