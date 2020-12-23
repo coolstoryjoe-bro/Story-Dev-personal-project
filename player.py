5fa7298a22a37a1a372dc7a087aa9a0a8518943a
Joseph Abad <75592216+coolstoryjoe-bro@users.noreply.github.com>
2020-12-22 23:45:25 -0800

    Add files via upload

--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+import cqcWeapons
+
 class Player:
     def __init__(self, health, defense, hit_rate, speed, damage):
         self.player_health = health
@@ -5,16 +7,20 @@ class Player:
         self.player_hit_rate = hit_rate
         self.player_speed = speed
         self.player_damage = damage
-        self.player_inventory_cqc = {
-            'Long Swords': '',
-            'Short Swords': '',
-            'Daggers': '',
-            'Staffs': '',
-            'Mace': '',
+        self.player_inventory_cqc = [{
+            'Long Swords': ['']},
+            {
+            'Short Swords': ['']},
+            {
+            'Daggers': ['']},
+            {
+            'Staffs': ['']},
+            {
+            'Mace': ['']},
+            {
 
-
-        }
-        self.equipped_inventory = {
+        }]
+        self.equipped_inventory = [{
             'Head': '',
             'Body': '',
             'Legs': '',
@@ -24,15 +30,34 @@ class Player:
             'CQC Weapon': '',
             'Ranged Weapon': '',
 
-        }
+        }]
+
+        cqc_weapons = [{'long swords': ['Basic Long Sword']},
+                       {'short swords': ['Basic Short Sword']},
+                       ]
 
     def equip_cqcWeapon(self):
         self.equipped_inventory['CQC Weapon'] = ''
 
     def unequip_cqcWeapon(self):
-        self.equipped_inventory['CQC Weapon'] = ''
-        if 
-        self.player_inventory_cqc['']
+        self.unequipped_cqcWeapon = self.equipped_inventory.pop[-2]('')
+        for weapons in range(len(cqc_weapons)):
+            for weapon_names in cqc_weapons[weapons]:
+                if weapon_names == self.unequipped_cqcWeapon:
+                    self.player_inventory_cqc[0]['Long Swords'] = [f'{self.unequipped_cqcWeapon}']
+                    break
+                elif weapon_names == self.unequipped_cqcWeapon:
+                    self.player_inventory_cqc[0]['Short Swords'] = [f'{self.unequipped_cqcWeapon}']
+                    break
+                elif weapon_names == self.unequipped_cqcWeapon:
+                    self.player_inventory_cqc[0]['Daggers'] = [f'{self.unequipped_cqcWeapon}']
+                    break
+                elif weapon_names == self.unequipped_cqcWeapon:
+                    self.player_inventory_cqc[0]['Staffs'] = [f'{self.unequipped_cqcWeapon}']
+                    break
+                elif weapon_names == self.unequipped_cqcWeapon:
+                    self.player_inventory_cqc[0]['Maces'] = [f'{self.unequipped_cqcWeapon}']
+                    break
 
 
     def use_equipped_cqcWeapon(self):
@@ -45,8 +70,6 @@ class Player:
         elif self.weapon_type == 'longSword' or self.weapon_type == 'mace':
             self.enemy_health -= self.base_damage
         print(f"You have done {self.base_damage} damage!")
-
-
 
 
 
